153f15d31bdbc0d982651289739f0d0a649d3b5b
lauragyr <laura.gyr@hotmail.com>
2017-12-07 09:29:14 +0100

    fixed function in formengenerator and made towergenerator

--- js/tower.js
+++ js/tower.js
@@ -1,13 +1,22 @@
-var muster = document.getElementById('towerbox');
+let muster = document.getElementById('towerbox');
 
 function towergenerator() {
-    var draw = muster.getContext('2d')
-
-    draw.fillRect(0,0,504,504);
-    draw.fillStyle = '#000000';
-
+    let draw = muster.getContext('2d')
+    let h = muster.height;
+    let w = muster.width;
 
     let buchstabenTEST = [[0,"A"], [63,"B"],[126,"C"], [189,"D"],[252,"E"], [315,"F"], [378,"G"], [441,"H"]];
+    let dictionary = [];
+    dictionary = {
+        0: "A",
+        63: "B",
+        126: "C",
+        189: "D",
+        252: "E",
+        315: "F",
+        378: "G",
+        441: "H"};
+    
     let zahlenTEST = [[0,"1"], [63,"2"],[126,"3"], [189,"4"],[252,"5"], [315,"6"], [378,"7"], [441,"8"]];
     let zahlen = [0, 63, 126, 189, 252, 315, 378, 441];
     let buchstaben = [0, 63, 126, 189, 252, 315, 378, 441];
@@ -26,9 +35,10 @@ function towergenerator() {
     console.log("was ist diese?: " + randTest2);
 */
 
+    draw.fillStyle = '#FFFFFF';
+    draw.fillRect(0,0,504,504);
+    console.log(dictionary[randBuchstaben], randZahlen/63 + 1);
+    document.getElementById("Koordinaten").innerHTML = dictionary[randBuchstaben] + " " + (randZahlen/63 + 1)
+    draw.fillStyle = '#000000';    
     draw.fillRect(randZahlen, randBuchstaben, 63, 63);  
-    draw.fillStyle = '#FFFFFF'; 
-
-
-
 }
